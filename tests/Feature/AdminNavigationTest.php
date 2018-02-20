e07158e38489cd2d89ab6558accac6ecdbaf7875
Brian <brian.cass@unc.edu>
2018-02-20 15:46:17 -0500

    CLINGEN-9 CLINGEN-10 Added tests

---
+++ tests/Feature/AdminNavigationTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+/**
+ * @group admin
+ */
+class AdminNavigationTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function setUp()
+    {
+        parent::setUp();
+        // \Artisan::call('db:seed');
+        $seeder = new \RolesAndPermissionsSeeder();
+        $seeder->run();
+        $this->u = factory(\App\User::class)->create();
+    }
+
+    /**
+     * @test
+     */
+    public function programmers_can_dashboard()
+    {
+        $this->u->assignRole('programmer');
+
+        $this->actingAs($this->u)
+            ->call('GET', '/admin/dashboard')
+            ->assertSee('Dashboard');
+    }
+
+    /**
+     * @test
+     */
+    public function admins_can_dashboard()
+    {
+        $this->u->assignRole('admin');
+
+        $this->actingAs($this->u)
+            ->call('GET', '/admin/dashboard')
+            ->assertSee('Dashboard');
+    }
+
+    /**
+     * @test
+     */
+    public function curators_can_not_see_dashboard()
+    {
+        $this->u->assignRole('curator');
+
+        $this->actingAs($this->u)
+            ->call('GET', '/admin/dashboard')
+            ->assertStatus(404);
+    }
+
+    /**
+     * @test
+     */
+    public function coordinators_can_not_see_dashboard()
+    {
+        $this->u->assignRole('coordinator');
+
+        $this->actingAs($this->u)
+            ->call('GET', '/admin/dashboard')
+            ->assertStatus(404);
+    }
+}
