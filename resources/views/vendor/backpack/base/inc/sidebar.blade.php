49baad6e2c6010cba9f185e8e41b89d7d046a14f
Brian <brian.cass@unc.edu>
2018-02-20 13:05:59 -0500

    CLINGEN-9 CLINGEN-10 committing backpack published files

---
+++ resources/views/vendor/backpack/base/inc/sidebar.blade.php
@@ -0,0 +1,24 @@
+@if (Auth::check())
+    <!-- Left side column. contains the sidebar -->
+    <aside class="main-sidebar">
+      <!-- sidebar: style can be found in sidebar.less -->
+      <section class="sidebar">
+        <!-- Sidebar user panel -->
+        @include('backpack::inc.sidebar_user_panel')
+
+        <!-- sidebar menu: : style can be found in sidebar.less -->
+        <ul class="sidebar-menu">
+          {{-- <li class="header">{{ trans('backpack::base.administration') }}</li> --}}
+          <!-- ================================================ -->
+          <!-- ==== Recommended place for admin menu items ==== -->
+          <!-- ================================================ -->
+          <li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard"></i> <span>{{ trans('backpack::base.dashboard') }}</span></a></li>
+
+
+          <!-- ======================================= -->
+          {{-- <li class="header">Other menus</li> --}}
+        </ul>
+      </section>
+      <!-- /.sidebar -->
+    </aside>
+@endif
