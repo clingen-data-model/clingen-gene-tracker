bcbdc49d0c18cbf6caa04f828d67e47e46c5582c
jward3 <jward3@email.unc.edu>
2018-03-10 12:56:18 -0500

    Add criteria table

---
+++ app/Http/Resources/PhenotypeResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Http\Resources\TopicResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PhenotypeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'mim_number' => $this->mim_number,
+            'topics' => TopicResource::collection($this->whenLoaded('topics'))
+        ];
+    }
+}
---
+++ resources/assets/js/components/Topics/CriteriaTable.vue
@@ -0,0 +1,34 @@
+<style>
+</style>
+<template>
+    <div class="criteria-table" style="line-height: 1">
+        <table class="table table-bordered">
+            <thead>
+                <tr><th>Criteria</th><th>Question</th></tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Assertion</td>
+                    <td><small class="short-line-height">Have assertions for more than one disease entity been reported for the gene of interest?</small></td>
+                </tr>
+                <tr>
+                    <td>Molecular mechanism</td>
+                    <td><small class="short-line-height">Are there differences in molecular mechanisms between the disease entities associated with the gene?</small></td>
+                </tr>
+                <tr>
+                    <td>Phenotypic variability</td>
+                    <td><small class="short-line-height">Have examples of intrafamilial and/or interfamilial phenotypic variability been reported for the disease entities associated with the gene in question?</small></td>
+                </tr>
+                <tr>
+                    <td>Inheritance Pattern</td>
+                    <td><small class="short-line-height">Do the disease entities associated with different inheritance patterns represent a continuum of disease or distinct disease entities?</small></td>
+                </tr>
+            </tbody>
+        </table>    
+    </div>
+</template>
+<script>
+    export default {
+        //component definition
+    }
+</script>
