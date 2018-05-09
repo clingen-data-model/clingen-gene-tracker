ef42948a6cbaa910bd84b666926216ebaac9f9c2
jward3 <jward3@email.unc.edu>
2018-05-09 15:44:33 -0400

    Update topic statuses table and seed

--- database/migrations/2018_04_17_133622_create_topic_statuses_table.php
+++ database/migrations/2018_04_17_133622_create_topic_statuses_table.php
@@ -16,6 +16,7 @@ class CreateTopicStatusesTable extends Migration
         Schema::create('topic_statuses', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name')->unique();
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
