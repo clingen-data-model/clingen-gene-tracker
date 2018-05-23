ca47e6b8853986d125ea8513fcc70c79e86fedcb
jward3 <jward3@email.unc.edu>
2018-05-23 17:17:09 -0400

    update for CD test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
         <nav class="navbar navbar-default navbar-expand-md navbar-light navbar-laravel {{ config('app.env') }}">
             <div class="container">
                 <a class="navbar-brand" href="/#/">
-                    {{ site_title() }}
+                    {{ site_title() }} - test deploy!
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
