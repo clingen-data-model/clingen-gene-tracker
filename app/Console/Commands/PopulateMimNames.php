3d1601a744efa829e5be8d540e72cacb67008b12
jward3 <jward3@email.unc.edu>
2018-08-14 16:06:05 -0400

    Update phenotype name backfill

--- app/Console/Commands/PopulateMimNames.php
+++ app/Console/Commands/PopulateMimNames.php
@@ -43,21 +43,22 @@ class PopulateMimNames extends Command
         $curations = Curation::with('phenotypes');
         $curations->each(function ($curation) use ($omim) {
             $curation->phenotypes->each(function ($pheno) use ($curation, $omim) {
-                // dump($curation->gene_symbol);
-                // dump($pheno->mim_number);
+                dump('$curation->gene_symbol: '.$curation->gene_symbol);
+                dump('$pheno->mim_number: '.$pheno->mim_number);
                 // dump($omim->getGenePhenotypes($curation->gene_symbol));
                 if (empty($pheno->name)) {
-                    dump($pheno->mim_number);
+                    // dump($pheno->mim_number);
                     $genePhenos = $omim->getGenePhenotypes($curation->gene_symbol);
-                    dump($genePhenos);
+                    // dump($genePhenos);
                     $genePheno = $genePhenos->filter(function ($gp) use ($pheno) {
+                        dump($gp->phenotypeMimNumber.' == '.$pheno->mim_number);
                         return $gp->phenotypeMimNumber == $pheno->mim_number;
                     })->first();
 
                     // dump($genePheno);
                     if ($genePheno) {
                         $pheno->update([
-                            'name' => $genePheno->titles->preferredTitle
+                            'name' => $genePheno->phenotype
                         ]);
                     }
                 }
