8fabaee1f97defa8a9a991d4b7b3b1181cdc48c6
jward3 <jward3@email.unc.edu>
2018-10-15 11:25:33 -0400

    Update dbimport so it will work with deployer dir structure

--- config/dbimport.php
+++ config/dbimport.php
@@ -5,4 +5,16 @@ return [
     'afterImport' => '\App\DbImport\ImportHooks@afterImport',
     'ignoreTables' => null,
     'whitelist' => null,
+    'prod_db' => [
+        'host' => env('PROD_DB_HOST'),
+        'database' => env('PROD_DB_DATABASE'),
+        'username' => env('PROD_DB_USERNAME'),
+        'password' => env('PROD_DB_PASSWORD'),
+    ],
+    'demo_db' => [
+        'host' => env('DEMO_DB_HOST'),
+        'database' => env('DEMO_DB_DATABASE'),
+        'username' => env('DEMO_DB_USERNAME'),
+        'password' => env('DEMO_DB_PASSWORD'),
+    ]
 ];
