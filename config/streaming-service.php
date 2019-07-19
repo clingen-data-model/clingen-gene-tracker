06975d01f3cd2b3a7a83b941ecbb96b48dc80ce3
jward3 <jward3@email.unc.edu>
2019-07-19 15:07:02 -0400

    Fix Name of env var for streaming-service.enable-push config

--- config/streaming-service.php
+++ config/streaming-service.php
@@ -2,7 +2,7 @@
 
 return [
     'driver' => env('STREAMING_SEVICE_ENABLE_PUSH', false) ? 'log' : env('STREAMING_SEVICE_DRIVER', 'kafka'),
-    'enable-push' => env('STREAMING_SEVICE_ENABLE_PUSH', false),
+    'enable-push' => env('STREAMING_SERVICE_ENABLE_PUSH', false),
     'cert-location' => env('KAFKA_CERT', '/etc/pki/tls/certs/kafka.web3demo.signed.crt'),
     'key-location' => env('KAFKA_KEY_LOCATION', '/etc/pki/tls/private/kafka.apache.key'),
     'ca-location' =>  env('KAFKA_CA_LOCATION', '/etc/pki/ca-trust/extracted/openssl/ca-kafka-cert'),
