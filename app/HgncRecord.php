f209d511762312951a63da3606a19bfcff6d7ebe
jward3 <jward3@email.unc.edu>
2019-09-19 15:42:17 -0400

    Extract central Record code to ExternalServiceRecord

--- app/HgncRecord.php
+++ app/HgncRecord.php
@@ -2,37 +2,9 @@
 
 namespace App;
 
-final class HgncRecord
+final class HgncRecord extends ExternalServiceRecord
 {
-    protected $attributes;
-
-    public function __construct($attributes)
-    {
-        $this->attributes = $attributes;
-    }
-
     public function getHgncId() {
         return (int)substr($this->attributes->hgnc_id, 5);
     }
-
-    public function getAttributes() {
-        return $this->attributes;
-    }
-
-    public function __get($key) {
-        $method = 'get'.camel_case($key);
-        if (method_exists($this, $method)) {
-            return $this->$method($method, $this);
-        }
-
-        if (isset($this->attributes->{$key})) {
-            return $this->attributes->{$key};
-        }
-
-        return $this->{$key};
-    }
-
-    
-
-    
 }
