bb6f38c203c40d7d55bb0f789549ff1b171ea183
jward3 <jward3@email.unc.edu>
2019-09-19 16:07:03 -0400

    Add MondoClient

--- app/Providers/ClientServiceProvider.php
+++ app/Providers/ClientServiceProvider.php
@@ -4,8 +4,10 @@ namespace App\Providers;
 
 use App\Clients\HgncClient;
 use App\Clients\OmimClient;
+use App\Clients\MondoClient;
 use App\Contracts\HgncClient as HgncClientContract;
 use App\Contracts\OmimClient as OmimClientContract;
+use App\Contracts\MondoClient as MondoClientContract;
 use GuzzleHttp\Client;
 use Illuminate\Support\ServiceProvider;
 
@@ -37,6 +39,16 @@ class ClientServiceProvider extends ServiceProvider
                 ]
             ]);
             return new HgncClient($guzzleClient);
+        });
+
+        $this->app->bind(MondoClientContract::class, function () {
+            $guzzleClient = new Client([
+                'base_uri' => 'https://www.ebi.ac.uk/ols/api/ontologies/mondo/',
+                'headers' => [
+                    'Accept' => 'application/json'
+                ]
+            ]);
+            return new MondoClient($guzzleClient);
         });
     }
 }
