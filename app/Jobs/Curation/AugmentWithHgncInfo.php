c03906229e1dd8faddebc5be74b4f3cce3fb0560
jward3 <jward3@email.unc.edu>
2019-09-25 21:20:50 -0400

    AugmentWithHgncInfo looks for prev_symbol in hgnc if not found, updates curation, and notifies coordinators

--- app/Jobs/Curation/AugmentWithHgncInfo.php
+++ app/Jobs/Curation/AugmentWithHgncInfo.php
@@ -9,6 +9,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use App\Exceptions\HttpNotFoundException;
+use App\Mail\Curations\GeneSymbolUpdated;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 
@@ -37,13 +38,37 @@ class AugmentWithHgncInfo implements ShouldQueue
     public function handle(HgncClient $hgncClient)
     {
         try {
-            $geneSymbolRecord = $hgncClient->fetchGeneSymbol($this->curation->gene_symbol);
-            $this->curation->update([
-                'hgnc_name' => $geneSymbolRecord->name,
-                'hgnc_id' => $geneSymbolRecord->hgnc_id
-            ]);
+            $this->updateCurationWithSymbol($hgncClient);
         } catch (HttpNotFoundException $e) {
-            throw $e;
+            try {
+                $this->updateCurationWithPrevSymbol($hgncClient);
+            } catch (HttpNotFoundException $e) {
+                throw $e;
+            }
+        }
+    }
+
+    private function updateCurationWithSymbol($hgncClient)
+    {
+        $geneSymbolRecord = $hgncClient->fetchGeneSymbol($this->curation->gene_symbol);
+        $this->curation->update([
+            'hgnc_name' => $geneSymbolRecord->name,
+            'hgnc_id' => $geneSymbolRecord->hgnc_id
+        ]);        
+    }
+
+    private function updateCurationWithPrevSymbol($hgncClient)
+    {
+        $oldGeneSymbol = $this->curation->gene_symbol;
+        $prevSymbolRecord = $hgncClient->fetchPreviousSymbol($this->curation->gene_symbol);
+        $this->curation->update([
+            'gene_symbol' => $prevSymbolRecord->symbol,
+            'hgnc_name' => $prevSymbolRecord->name,
+            'hgnc_id' => $prevSymbolRecord->hgnc_id
+        ]);
+
+        foreach ($this->curation->expertPanel->coordinators as $coordinator) {
+            \Mail::to($coordinator)->send(new GeneSymbolUpdated($this->curation, $oldGeneSymbol));
         }
     }
 }
--- tests/Unit/Jobs/Curation/AugmentWithHgncInfoTest.php
+++ tests/Unit/Jobs/Curation/AugmentWithHgncInfoTest.php
@@ -2,12 +2,15 @@
 
 namespace Tests\Unit\Job\Curation;
 
+use App\User;
 use App\Curation;
 use Tests\TestCase;
+use App\ExpertPanel;
 use OutOfBoundsException;
 use App\Contracts\HgncClient;
 use App\Exceptions\HttpNotFoundException;
 use App\Jobs\Curation\AugmentWithHgncInfo;
+use App\Mail\Curations\GeneSymbolUpdated;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 /**
@@ -20,8 +23,14 @@ class AugmentWithHgncInfoTest extends TestCase
     public function setUp():void
     {
         parent::setUp();
+        $this->ep = factory(ExpertPanel::class)->create();
+        $this->coord = factory(User::class)->create();
+        $this->coord->expertPanels()->attach([$this->ep->id => [
+            'is_coordinator' => 1
+        ]]);
         $this->curation = factory(Curation::class)->create([
-            'gene_symbol' => 'TH'
+            'gene_symbol' => 'TH',
+            'expert_panel_id' => $this->ep->id
         ]);
         $this->hgncClient = $this->getMockBuilder(HgncClient::class)
                                 ->getMock();
@@ -40,6 +49,8 @@ class AugmentWithHgncInfoTest extends TestCase
     {
         $this->curation->gene_symbol = 'MLTN1';
         $this->hgncClient->method('fetchGeneSymbol')
+                        ->will($this->throwException(new HttpNotFoundException()));
+        $this->hgncClient->method('fetchPreviousSymbol')
                         ->will($this->throwException(new HttpNotFoundException()));
 
         app()->instance(HgncClient::class, $this->hgncClient);
@@ -64,4 +75,39 @@ class AugmentWithHgncInfoTest extends TestCase
         $this->assertDatabaseHas('curations', ['gene_symbol' => 'TH', 'hgnc_name' => 'tyrosine hydroxylase', 'hgnc_id' => 11782]);
     }
     
+    /**
+     * @test
+     */
+    public function updates_previous_symbol_with_new_symbol_symbol_changed()
+    {
+        $this->curation->gene_symbol = 'MLTN1';
+        $this->hgncClient->method('fetchGeneSymbol')
+                        ->will($this->throwException(new HttpNotFoundException()));
+        $this->hgncClient->method('fetchPreviousSymbol')
+                        ->willReturn((object)[
+                            'hgnc_id' => 11782,
+                            'symbol' => 'MLTN2',
+                            'name' => 'Milton Dog',
+                            'prev_symbol' => 'MLTN1'
+                        ]);
+
+        app()->instance(HgncClient::class, $this->hgncClient);
+
+        \Mail::fake();
+        $job = new AugmentWithHgncInfo($this->curation);
+        $job->handle($this->hgncClient);
+
+        $this->assertDatabaseHas('curations', [
+            'hgnc_id' => 11782,
+            'gene_symbol' => 'MLTN2',
+            'hgnc_name' => 'Milton Dog'
+        ]);
+
+        \Mail::assertSent(GeneSymbolUpdated::class, function ($mail) {
+            return $mail->hasTo($this->coord->email);
+        });
+    }
+    
+
+
 }
