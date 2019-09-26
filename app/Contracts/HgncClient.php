bb87007a1bd36de02fbe0ffe3aef60d4b5658a68
jward3 <jward3@email.unc.edu>
2019-09-25 21:19:43 -0400

    Add FetchPreviousSymbol to HgncClient contract and implementation

--- app/Contracts/HgncClient.php
+++ app/Contracts/HgncClient.php
@@ -5,4 +5,5 @@ namespace App\Contracts;
 interface HgncClient
 {
     public function fetchGeneSymbol(string $geneSymbol):object;
+    public function fetchPreviousSymbol(string $geneSymbol):object;
 }
