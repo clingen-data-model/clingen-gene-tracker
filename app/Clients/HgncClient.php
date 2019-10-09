3d032ddcb0ce99a0cb21ad78a8e73e9af75c98fd
jward3 <jward3@email.unc.edu>
2019-10-09 19:10:43 -0400

    Add fetchHgncId to HgncClient and refactor for reuse

--- app/Clients/HgncClient.php
+++ app/Clients/HgncClient.php
@@ -4,9 +4,11 @@ namespace App\Clients;
 
 use App\HgncRecord;
 use GuzzleHttp\Client;
+use App\Contracts\GeneSymbolUpdate as GeneSymbolUpdateContract;
 use App\Exceptions\HttpNotFoundException;
 use App\Contracts\HgncClient as HgncClientContract;
 use App\Exceptions\HttpUnexpectedResponseException;
+use App\ValueObjects\GeneSymbolUpdate;
 
 class HgncClient implements HgncClientContract
 {
@@ -17,35 +19,57 @@ class HgncClient implements HgncClientContract
         $this->guzzleClient = $guzzleClient;
     }
 
-    public function fetchGeneSymbol(string $geneSymbol):object
+    public function fetch($key, $value):object
     {
-        $response = $this->guzzleClient->request('GET', '/fetch/symbol/'.$geneSymbol);
+        $response = $this->guzzleClient->request('GET', '/fetch/'.$key.'/'.$value);
         $responseObj = json_decode($response->getBody()->getContents());
         if ($responseObj->response->numFound == 0) {
-            throw new HttpNotFoundException('Gene symbol '.$geneSymbol.' not found in HGNC API.');
+            throw new HttpNotFoundException($key.' '.$value.' not found in HGNC API.');
         }
 
         if ($responseObj->response->numFound > 1) {
-            throw new HttpUnexpectedResponseException('Search for gene symbol '.$geneSymbol.'resulted in '.$responseObj->response->numFound.' records found in HGNC API.');
+            throw new HttpUnexpectedResponseException('Search for '.$key.' '.$value.'resulted in '.$responseObj->response->numFound.' records found in HGNC API.');
         }
 
         return new HgncRecord($responseObj->response->docs[0]);
+    }
+
+    public function fetchHgncId(string $hgncId):object
+    {
+        return $this->fetch('hgnc_id', $hgncId);
+    }
+
+    public function fetchGeneSymbol(string $geneSymbol):object
+    {
+        return $this->fetch('symbol', $geneSymbol);
     }
 
     public function fetchPreviousSymbol(string $geneSymbol):object
     {
-        $response = $this->guzzleClient->request('GET', '/fetch/prev_symbol/'.$geneSymbol);
-        $responseObj = json_decode($response->getBody()->getContents());
-        if ($responseObj->response->numFound == 0) {
-            throw new HttpNotFoundException('Gene symbol '.$geneSymbol.' not found in HGNC API.');
+        return $this->fetch('prev_symbol', $geneSymbol);
+    }
+    
+    public function fetchGeneSymbolUpdate(string $geneSymbol):GeneSymbolUpdateContract
+    {
+        try {
+            $symbolRecord = $this->fetchGeneSymbol($geneSymbol);
+        } catch (HttpNotFoundException $th) {
+            try {
+                $symbolRecord = $this->fetchPreviousSymbol($geneSymbol);
+            } catch (HttpNotFoundException $th) {
+                return new GeneSymbolUpdate($geneSymbol, null);
+            }
         }
 
-        if ($responseObj->response->numFound > 1) {
-            throw new HttpUnexpectedResponseException('Search for gene symbol '.$geneSymbol.'resulted in '.$responseObj->response->numFound.' records found in HGNC API.');
-        }
-
-        return new HgncRecord($responseObj->response->docs[0]);
+        return new GeneSymbolUpdate($geneSymbol, $symbolRecord->symbol);
     }
 
+    public function fetchSymbolUpdateForHgncId($hgncId)
+    {
+        $record = $this->fetchHgncId($hgncId);
+        if ($record->hasPrevSymbol()) {
+            return new GeneSymbolUpdate($record->symbol, $record->prev_symbol);
+        }
+    }
     
 }
--- app/Contracts/HgncClient.php
+++ app/Contracts/HgncClient.php
@@ -2,8 +2,11 @@
 
 namespace App\Contracts;
 
+use App\Contracts\GeneSymbolUpdate;
+
 interface HgncClient
 {
     public function fetchGeneSymbol(string $geneSymbol):object;
     public function fetchPreviousSymbol(string $geneSymbol):object;
+    public function fetchGeneSymbolUpdate(string $geneSymbol):GeneSymbolUpdate;
 }
