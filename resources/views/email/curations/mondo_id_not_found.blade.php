793a611113c343ad194164977b9d7052f8f2045e
jward3 <jward3@email.unc.edu>
2019-10-13 21:31:50 -0400

    Update MondoID not found email template

--- resources/views/email/curations/mondo_id_not_found.blade.php
+++ resources/views/email/curations/mondo_id_not_found.blade.php
@@ -1,13 +1,14 @@
-<h2>
-    The MonDO ID {{$curation->mondo_id}} entered for 
-    a <a href="{{url('/#/curations/'.$curation->id)}}">curation {{$curation->id}}</a> was not found
-</h2>
-
 <p>
     The MonDO ID {{$curation->mondo_id}} used for 
-    <a href="{{url('/#/curations/'.$curation->id)}}">one of your curatations</a>
+    <a href="{{url('/#/curations/'.$curation->id)}}"> your curatation with id {{$curation->id}}</a>
     could not be found in the Monarch Disease Ontology.
 </p>
+
+<p>Please check the following:</p>
+<ol>
+    <li>The MonDO ID is not correctly formatted.  MonDO IDs should be formatted <strong>MONDO:123453</strong> with no spaces or other puntation</li>
+    <li>Leading zeros are required.</li>
+</ol>
 
 <p>
     Please consult MonDO and update the curation with an existing MonDO ID.
