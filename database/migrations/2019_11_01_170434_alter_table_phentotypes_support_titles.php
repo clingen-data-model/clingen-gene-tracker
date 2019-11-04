841d654e3e600d962665ae4fdcdfd060521df52d
jward3 <jward3@email.unc.edu>
2019-11-04 17:44:57 -0500

    Add omim_entry to curations table

--- database/migrations/2019_11_01_170434_alter_table_phentotypes_support_titles.php
+++ database/migrations/2019_11_01_170434_alter_table_phentotypes_support_titles.php
@@ -14,7 +14,7 @@ class AlterTablePhentotypesSupportTitles extends Migration
     public function up()
     {
         Schema::table('phenotypes', function (Blueprint $table) {
-            $table->json('omim_entry')->nullable();
+            $table->json('omim_entry')->nullable()->after('name');
         });
     }
 
