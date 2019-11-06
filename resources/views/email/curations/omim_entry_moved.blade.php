5997715c87826959327a5e643431a9b6ad77185a
jward3 <jward3@email.unc.edu>
2019-11-06 12:57:11 -0500

    Add missing link/text to omim_entry_moved email

--- resources/views/email/curations/omim_entry_moved.blade.php
+++ resources/views/email/curations/omim_entry_moved.blade.php
@@ -3,7 +3,7 @@
 <p>
     OMIM has moved the entry for a phenotype associated with one of your <a href="{{url('/#/curations/'.$curation->id)}}">curations</a>.
     The phenotype {{$oldName}} with MIM Number {{$oldMimNumber}} has been moved to the MIM number {{$phenotype->mim_number}} with preferred title of "{{$phenotype->name}}" 
-    and one of your  has been automatically updated.
+    and one of your <a href="{{url('/#/curations/'.$curation->id)}}">curation</a> has been automatically updated.
 
     You may want to <a href="https://www.omim.org/entry/{{$phenotype->mim_number}}">review the new OMIM record</a>.
 </p>
