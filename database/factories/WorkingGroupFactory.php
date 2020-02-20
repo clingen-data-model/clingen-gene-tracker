b35718116b2418a8da1d4ed875236735f655a561
TJ Ward <jward3@email.unc.edu>
2020-02-20 18:44:39 -0500

    Add affiliation_id to WorkingGroupFactory so tests will pass

--- database/factories/WorkingGroupFactory.php
+++ database/factories/WorkingGroupFactory.php
@@ -5,5 +5,6 @@ use Faker\Generator as Faker;
 $factory->define(App\WorkingGroup::class, function (Faker $faker) {
     return [
         'name' => $faker->unique()->name,
+        'affiliation_id' => null
     ];
 });
