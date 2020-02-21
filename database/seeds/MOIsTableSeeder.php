1aa5e9a0d1c0792a6bef461c2270a854a801e703
TJ Ward <jward3@email.unc.edu>
2020-02-21 10:26:25 -0500

    Add HP:000005 and Other to MOIsTableSeeder

--- database/seeds/MOIsTableSeeder.php
+++ database/seeds/MOIsTableSeeder.php
@@ -11,6 +11,14 @@ class MOIsTableSeeder extends Seeder
     public function run()
     {
         $mois = json_decode(file_get_contents(base_path('files/moi.json')));
+        ModeOfInheritance::updateOrCreate(
+            ['hp_id' => 'HP:0000005'],
+            [
+                'name' => 'Undetermined mode of inheritance',
+                'parent_id' => null,
+                'hp_uri' => 'http://purl.obolibrary.org/obo/HP_0000005'
+            ]
+        );
         collect($mois)->flatten()->each(function ($moi) {
             $parent = ModeOfInheritance::where('hp_uri', $moi->parentUri)->first();
             $hpId = preg_replace('%http://purl.obolibrary.org/obo/HP_%', '', $moi->uri);
@@ -21,5 +29,13 @@ class MOIsTableSeeder extends Seeder
                 'parent_id' => ($parent) ? $parent->id : null,
             ]);
         });
+        ModeOfInheritance::updateOrCreate(
+            ['hp_id' => 'HP:0000000'],
+            [
+                'name' => 'Other',
+                'parent_id' => null,
+                'hp_uri' => 'http://purl.obolibrary.org/obo/HP_0000000'
+            ]
+        );
     }
 }
