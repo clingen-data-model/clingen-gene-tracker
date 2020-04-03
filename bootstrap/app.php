22d51332d08de57d686f246edd8bb6fc069992ab
TJ Ward <jward3@email.unc.edu>
2020-04-03 13:28:57 -0400

    Check for existence of .env before loading files

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -71,34 +71,4 @@ if (env('APP_STORAGE_PATH')) {
 }
 
 
-Dotenv::create(__DIR__.'/..')->load();
-if (env('APP_STORAGE_PATH')) {
-    // get the current working directory b/c it could be frigging anywhere
-    $cwd = getcwd();
-    // change to the base path so we can handle paths relative to the location of .env
-    chdir(base_path());
-    // Get the real path in the .env
-    $storagePath = realpath(base_path(env('APP_STORAGE_PATH', 'storage')));
-    // change back to original working directory
-    chdir($cwd);
-
-    $app->useStoragePath($storagePath);
-}
-
-
-Dotenv::create(__DIR__.'/..')->load();
-if (env('APP_STORAGE_PATH')) {
-    // get the current working directory b/c it could be frigging anywhere
-    $cwd = getcwd();
-    // change to the base path so we can handle paths relative to the location of .env
-    chdir(base_path());
-    // Get the real path in the .env
-    $storagePath = realpath(base_path(env('APP_STORAGE_PATH', 'storage')));
-    // change back to original working directory
-    chdir($cwd);
-
-    $app->useStoragePath($storagePath);
-}
-
-
 return $app;
