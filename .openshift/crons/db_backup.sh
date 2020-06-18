629186dfee40242dd7ffc3fb68b7c60198d4125d
TJ Ward <jward3@email.unc.edu>
2020-06-18 10:12:42 -0400

    update backup crons

---
+++ .openshift/crons/db_backup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+TODAY=`date +"%Y-%m-%-d"`
+NOW=`date +"%Y-%m-%d-%H:%M:%S"`
+
+BACKUP_RETAIN_DAYS=30
+
+mkdir -p ${BACKUP_PATH}/${TODAY}
+echo "Backup started for database ${DATABASE_NAME}"
+
+mysqldump -u root \
+    --all-databases | gzip > ${BACKUP_PATH}/${TODAY}/${NOW}.sql.gz
+
+if [ $? -eq 0 ]; then
+  echo "db backup successful"
+else
+  echo "Error found during backup"
+  exit 1
+fi
+
+REMOVE_DATE=`date +"%Y-%m-%d" --date="$BACKUP_RETAIN_DAYS days ago"`
+
+if [ ! -z ${BACKUP_PATH} ];
+then
+    cd ${BACKUP_PATH}
+    if [ ! -z ${REMOVE_DATE} ] && [ -d ${REMOVE_DATE} ];
+    then
+        echo "Remove ${REMOVE_DATE}"
+        rm -rf ${REMOVE_DATE}
+    else
+    fi
+else
+fi
