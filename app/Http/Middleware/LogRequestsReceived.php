29e30fd08685e90cdf67aee5ae4bb9de35174a2a
TJ Ward <jward3@email.unc.edu>
2020-09-21 13:12:19 -0400

    Add middleware to log requests

---
+++ app/Http/Middleware/LogRequestsReceived.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class LogRequestsReceived
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        logDebug('Request received');
+
+        return $next($request);
+    }
+}
