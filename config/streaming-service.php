474eb64aef0468d9887c2ef14de78f78c8160e29
TJ Ward <jward3@email.unc.edu>
2020-11-02 12:22:00 -0500

    White space

--- config/streaming-service.php
+++ config/streaming-service.php
@@ -6,7 +6,7 @@ return [
     'enable-push' => env('STREAMING_SERVICE_ENABLE_PUSH', false),
     'cert-location' => env('KAFKA_CERT', '/etc/pki/tls/certs/kafka.web3demo.signed.crt'),
     'key-location' => env('KAFKA_KEY_LOCATION', '/etc/pki/tls/private/kafka.apache.key'),
-    'ca-location' =>  env('KAFKA_CA_LOCATION', '/etc/pki/ca-trust/extracted/openssl/ca-kafka-cert'),
+    'ca-location' => env('KAFKA_CA_LOCATION', '/etc/pki/ca-trust/extracted/openssl/ca-kafka-cert'),
     'ssl-key-password' => env('KAFKA_KEY_PASSWORD', null),
     'kafka_username' => env('KAFKA_USERNAME'),
     'kafka_password' => env('KAFKA_PASSWORD'),
