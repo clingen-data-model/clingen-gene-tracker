6856972eee8c8d7ee5f2be99d2a6d95b9eb49bfe
TJ Ward <jward3@email.unc.edu>
2021-02-25 10:30:10 -0500

    Handle case in gene_symbol_updated.blade.php where  does not have hgnc_id

--- resources/views/email/digest/gene_symbol_updated.blade.php
+++ resources/views/email/digest/gene_symbol_updated.blade.php
@@ -8,7 +8,9 @@
         <li>
             <strong>{{$oldGeneSymbol}}</strong> to <strong>{{$curation['gene_symbol']}}</strong>
             - <a href="url('/#/curations/{{$curation['id']}}')">curation {{$curation['id']}}</a> 
-            - <a href="https://www.genenames.org/data/gene-symbol-report/#!/hgnc_id/HGNC:{{$curation['hgnc_id']}}">HGNC record</a>
+            @if(isset($curation['hgnc_id']))
+                - <a href="https://www.genenames.org/data/gene-symbol-report/#!/hgnc_id/HGNC:{{$curation['hgnc_id']}}">HGNC record</a>
+            @endif
         </li>
     @endforeach
 </ul>
