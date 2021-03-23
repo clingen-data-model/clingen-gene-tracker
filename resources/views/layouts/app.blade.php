c65c8c6393b6ddb800dd7a4d8920a31c0f01c5d6
TJ Ward <jward3@email.unc.edu>
2021-03-23 14:09:51 -0400

    Remove unnecessary close img tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,7 +41,7 @@
             <div class="container">
                 <a class="navbar-brand" href="/#/">
                     {{-- {{ site_title() }} --}}
-                    <img src="/images/clingen_logo_75w.png"></img>
+                    <img src="/images/clingen_logo_75w.png">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
