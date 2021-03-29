0ef48a8aad16a5846b41f34c813c419350e8ed4d
TJ Ward <jward3@email.unc.edu>
2021-03-29 15:09:42 -0400

    Fix the MOIs table seeder to handle curatable flag.

--- database/seeds/MOIsTableSeeder.php
+++ database/seeds/MOIsTableSeeder.php
@@ -29,6 +29,13 @@ class MOIsTableSeeder extends Seeder
                 'hp_uri' => $moi->uri,
                 'hp_id' => 'HP:'.$hpId,
                 'parent_id' => ($parent) ? $parent->id : null,
+                'curatable' => in_array('hp_id', [
+                    'HP:0000005',
+                    'HP:0000006',
+                    'HP:0000007',
+                    'HP:0001417',
+                    'HP:0032113'
+                ]) ? 1 : 0
             ]);
         });
         ModeOfInheritance::updateOrCreate(
@@ -38,6 +45,7 @@ class MOIsTableSeeder extends Seeder
                 'abbreviation' => 'OTHER',
                 'parent_id' => null,
                 'hp_uri' => 'http://purl.obolibrary.org/obo/HP_0000000',
+                'curatable' => 0
             ]
         );
     }
