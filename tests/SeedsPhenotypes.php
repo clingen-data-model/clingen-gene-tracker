26f0c190ac7e9c927d056573d1d3c3fffdc7ea37
TJ Ward <jward3@email.unc.edu>
2021-04-28 17:28:04 -0400

    Add SeedsPhenotypes trait

---
+++ tests/SeedsPhenotypes.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests;
+
+use App\Phenotype;
+
+trait SeedsPhenotypes
+{
+    public function seedPhenotypes($data = null)
+    {
+        if (!$data) {
+            $data = [
+                [
+                    'name' => 'Cardiomyopathy, hypertrophic, 2',
+                    'mim_number' => 115195,
+                ]
+                // [
+                //     'name' => 'Neurofibromatosis, type 2',
+                //     'mim_number' => 101000
+                // ],
+                // [
+                //     'name' => ' Retinoblastoma',
+                //     'mim_number' => 180200
+                // ]
+            ];
+        }
+        
+        $phenotypes = collect();
+        foreach ($data as $d) {
+            $ph = Phenotype::create($d);
+            $phenotypes->push($ph);
+        }
+
+        return $phenotypes;
+    }
+}
