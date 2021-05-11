f69834278537b90965dbbdff0c786b9fcd13986a
TJ Ward <jward3@email.unc.edu>
2021-05-11 12:19:01 -0400

    Fix typo in streaming-service config

--- config/streaming-service.php
+++ config/streaming-service.php
@@ -2,7 +2,7 @@
 
 return [
     'broker' => env('STREAMING_SERVICE_BROKER', 'exchange.clinicalgenome.org:9093'),
-    'driver' => env('STREAMING_SEVICE_ENABLE_PUSH', false) ? 'log' : env('STREAMING_SEVICE_DRIVER', 'kafka'),
+    'driver' => env('STREAMING_SERVICE_ENABLE_PUSH', false) ? env('STREAMING_SEVICE_DRIVER', 'kafka') : 'log',
     'enable-push' => env('STREAMING_SERVICE_ENABLE_PUSH', false),
     'cert-location' => env('KAFKA_CERT', '/etc/pki/tls/certs/kafka.web3demo.signed.crt'),
     'key-location' => env('KAFKA_KEY_LOCATION', '/etc/pki/tls/private/kafka.apache.key'),
