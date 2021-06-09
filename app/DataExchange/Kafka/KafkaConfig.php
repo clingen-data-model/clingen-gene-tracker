426cfd4cf2f588552bf906b5d05d72d9afd69992
TJ Ward <jward3@email.unc.edu>
2021-06-09 10:33:32 -0400

    Clarify KafkaConfig class to use: * Update App\DataExchange\Kafka\KafkaConfig to match App\DataExchange\KafkaConfig. * Remove App\DataExchange\KafkaConfig. * Use App\DataExchange\Kafka\KafkaConfig in DataExchangeServiceProvider.

--- app/DataExchange/Kafka/KafkaConfig.php
+++ app/DataExchange/Kafka/KafkaConfig.php
@@ -4,22 +4,26 @@ namespace App\DataExchange\Kafka;
 
 use Illuminate\Support\Facades\Log;
 use App\DataExchange\Exceptions\StreamingServiceException;
+use App\DataExchange\Kafka\KafkaEnvValidator;
 
 class KafkaConfig
 {
     private $conf;
 
-    public function __construct($group = null)
+    public function __construct(KafkaEnvValidator $validator, $group = null)
     {
+        $validator();
         $this->conf = new \RdKafka\Conf();
         $this->setGroup($group);
 
         // Initial list of Kafka brokers
-        $this->conf->set('metadata.broker.list', config("streaming-service.broker"));
+        $this->conf->set('metadata.broker.list', config("dx.broker"));
         $this->conf->set('log_level', (string) LOG_DEBUG);
 
         // security config
-        $this->configureAuth();
+        if (!app()->environment('testing')) {
+            $this->configSecurity();
+        }
         
         $this->conf->setErrorCb(function ($kafka, $err, $reason) {
             throw new StreamingServiceException("Kafka producer error: ".rd_kafka_err2str($err)." (reason: ".$reason.')');
@@ -52,29 +56,24 @@ class KafkaConfig
         return $this->conf;
     }
 
-    private function configureAuth()
+    private function configSecurity()
     {
-        if (app()->environment('testing')) {
-            return;
-        }
-
         if (config('dx.broker') == 'exchange.clinicalgenome.org:9093') {
             $this->conf->set('security.protocol', 'ssl');
             $this->conf->set('ssl.certificate.location', config('dx.cert-location'));
             $this->conf->set('ssl.key.location', config('dx.key-location'));
             $this->conf->set('ssl.ca.location', config('dx.ca-location', '/etc/pki/ca-trust/extracted/openssl/ca-kafka-cert'));
-    
+
             if (config('dx.ssl-key-password', null)) {
                 $this->conf->set('ssl.key.password', config('dx.ssl-key-password', null));
             }
 
             return;
         }
-
+        
         $this->conf->set('security.protocol', 'sasl_ssl');
         $this->conf->set('sasl.mechanism', 'PLAIN');
         $this->conf->set('sasl.username', config('dx.dx_username'));
         $this->conf->set('sasl.password', config('dx.dx_password'));
     }
-    
 }
