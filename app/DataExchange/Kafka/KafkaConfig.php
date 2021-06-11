54cdb9a6828b28adb77eaa816513fcb84765ab5d
TJ Ward <jward3@email.unc.edu>
2021-06-11 08:33:14 -0400

    Fix dx config group call in KafkaConfig.

--- app/DataExchange/Kafka/KafkaConfig.php
+++ app/DataExchange/Kafka/KafkaConfig.php
@@ -41,7 +41,7 @@ class KafkaConfig
     }
     public function setGroup($group = null)
     {
-        $group = $group ? $group : config('dx.dx_group', 'unc_demo');
+        $group = $group ? $group : config('dx.group', 'unc_demo');
         $this->conf->set('group.id', $group);
     }
 
