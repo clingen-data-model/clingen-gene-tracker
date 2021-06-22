ac41b74da588f898e5e4782318841538d9180e76
TJ Ward <jward3@email.unc.edu>
2021-06-22 07:37:19 -0400

    Remove dump message when producing to kafka.

--- app/DataExchange/Kafka/KafkaProducer.php
+++ app/DataExchange/Kafka/KafkaProducer.php
@@ -49,7 +49,6 @@ class KafkaProducer implements MessagePusher
 
     public function push(string $message, $uuid = null)
     {
-        dump("pushsing $message on ".$this->topic->getName());
         if (!$this->topic) {
             throw new StreamingServiceException('You must set a topic on the Producer before you can use KafkaProducer::produce');
         }
