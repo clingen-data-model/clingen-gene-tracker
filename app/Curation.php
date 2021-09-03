e31bf2b4974b71208c9d0a7d4d45522b315ebc92
TJ Ward <jward3@email.unc.edu>
2021-09-03 16:25:38 -0400

    Remove duplicate use statement

--- app/Curation.php
+++ app/Curation.php
@@ -20,7 +20,6 @@ use Venturecraft\Revisionable\RevisionableTrait;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * @property Classification $currentClassificiation
