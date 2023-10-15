e0bd29641ac7a130ea5ec96345d1d5a35bb1b24b
Shift <shift@laravelshift.com>
2023-10-15 17:50:14 +0000

    Convert string references to `::class`
    PHP 5.5.9 adds the new static `class` property which provides the fully qualified class name. This is preferred over using strings for class names since the `class` property references are checked by PHP.

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -19,18 +19,18 @@ Route::prefix(config('backpack.base.route_prefix', 'admin'))->middleware('web', 
     Route::get('/user/{id}/reactivate', [App\Http\Controllers\Admin\UserCrudController::class, 'reactivate'])
         ->name('user-reactivate');
 
-    Route::crud('user', 'App\Http\Controllers\Admin\UserCrudController');
-    Route::crud('aff', 'App\Http\Controllers\Admin\AffiliationCrudController');
-    Route::crud('expert-panel', 'App\Http\Controllers\Admin\ExpertPanelCrudController');
-    Route::crud('curation-status', 'App\Http\Controllers\Admin\CurationStatusCrudController');
-    Route::crud('working-group', 'App\Http\Controllers\Admin\WorkingGroupCrudController');
-    Route::crud('curation-type', 'App\Http\Controllers\Admin\CurationTypeCrudController');
-    Route::crud('rationale', 'App\Http\Controllers\Admin\RationaleCrudController');
-    Route::crud('email', 'App\Http\Controllers\Admin\EmailCrudController');
-    Route::crud('notification', 'App\Http\Controllers\Admin\NotificationCrudController');
-    Route::crud('upload-category', 'App\Http\Controllers\Admin\UploadCategoryCrudController');
-    Route::crud('moi', 'App\Http\Controllers\Admin\MoiCrudController');
-    Route::crud('api-client', 'App\Http\Controllers\Admin\ApiClientCrudController');
+    Route::crud('user', \App\Http\Controllers\Admin\UserCrudController::class);
+    Route::crud('aff', \App\Http\Controllers\Admin\AffiliationCrudController::class);
+    Route::crud('expert-panel', \App\Http\Controllers\Admin\ExpertPanelCrudController::class);
+    Route::crud('curation-status', \App\Http\Controllers\Admin\CurationStatusCrudController::class);
+    Route::crud('working-group', \App\Http\Controllers\Admin\WorkingGroupCrudController::class);
+    Route::crud('curation-type', \App\Http\Controllers\Admin\CurationTypeCrudController::class);
+    Route::crud('rationale', \App\Http\Controllers\Admin\RationaleCrudController::class);
+    Route::crud('email', \App\Http\Controllers\Admin\EmailCrudController::class);
+    Route::crud('notification', \App\Http\Controllers\Admin\NotificationCrudController::class);
+    Route::crud('upload-category', \App\Http\Controllers\Admin\UploadCategoryCrudController::class);
+    Route::crud('moi', \App\Http\Controllers\Admin\MoiCrudController::class);
+    Route::crud('api-client', \App\Http\Controllers\Admin\ApiClientCrudController::class);
     Route::get('api-client/{id}/create-token', [ApiClientCrudController::class, 'createToken']);
 }); // this should be the absolute last line of this file
 
