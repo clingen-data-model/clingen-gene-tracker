a2b7699d298b0102e4a1fa6a3a56a87ae757decc
Shift <shift@laravelshift.com>
2023-10-20 17:06:45 +0000

    Add PHP type hints

--- app/Http/Requests/Api/StoreCurationCurationStatusRequest.php
+++ app/Http/Requests/Api/StoreCurationCurationStatusRequest.php
@@ -11,7 +11,7 @@ class StoreCurationCurationStatusRequest extends FormRequest
      *
      * @return array
      */
-    public function rules()
+    public function rules(): array
     {
         return [
 'curation_status_id' => [
--- app/Http/Requests/Api/UpdateCurationCurationStatusRequest.php
+++ app/Http/Requests/Api/UpdateCurationCurationStatusRequest.php
@@ -11,7 +11,7 @@ class UpdateCurationCurationStatusRequest extends FormRequest
      *
      * @return array
      */
-    public function rules()
+    public function rules(): array
     {
         return ['status_date' => [
                 'date_format:Y-m-d',
