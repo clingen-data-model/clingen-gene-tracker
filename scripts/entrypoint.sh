4b5049d59f6a7251d51ec775251540fc10ef706e
Bradford Powell <bpow@drpowell.org>
2023-12-19 13:23:06 -0500

    await availability of db before running migrations

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -16,9 +16,6 @@ if [[ -v APP_DO_INIT || ! -d vendor ]]; then
     composer dump-autoload
 fi
 
-echo "Running migrations..."
-php artisan migrate --force --no-interaction
-
 echo "Making passport keys (if they do not already exist)"
 php artisan passport:keys || echo "... keys were probably already there"
 
@@ -30,6 +27,12 @@ if [[ ${APP_KEY:-invalid} != base64* ]]; then
     touch .env # make sure we have an .env file soe key:generate doesn't complain
     grep APP_KEY=base64 .env >/dev/null || php artisan key:generate -q
 fi
+
+echo "Awaiting connection to database"
+/srv/app/scripts/awaitdb.bash
+
+echo "Running migrations..."
+php artisan migrate --force --no-interaction
 
 if [[ $env != "local" ]]; then
     echo "Caching configuration..."
