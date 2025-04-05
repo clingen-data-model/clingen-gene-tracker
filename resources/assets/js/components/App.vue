450eae773e1cbbb697e430a2ae2204ac2df68d92
Bradford Powell <bpow@drpowell.org>
2025-04-05 10:53:26 -0400

    or maybe nuxtui?

---
+++ resources/assets/js/components/App.vue
@@ -0,0 +1,56 @@
+<style></style>
+<template>
+    <UApp>
+        <NavBar />
+        <div class="mt-2">
+            <div class="clingen-app-container container">
+<!--                 <router-view></router-view> -->
+            </div>
+            <UProgress mode="indeterminate" v-show="loading"
+                style="height: 6px; position:fixed; top:0; left:0; right:0; border-radius: 0" />
+            <Alerts></Alerts>
+        </div>
+    </UApp>
+</template>
+<script>
+import { mapActions } from 'vuex'
+import Alerts from '@/components/Alerts.vue'
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+    methods: {
+        ...mapActions('curationStatuses', {
+            getAllCurationStatuses: 'getAllItems'
+        }),
+        ...mapActions('rationales', {
+            getAllRationales: 'getAllItems'
+        }),
+        ...mapActions({
+            getFeatures: 'getFeatures'
+        })
+    },
+    components: {
+        Alerts,
+        NavBar,
+        ProgressBar,
+    },
+    data() {
+        return {
+            loading: true
+        }
+    },
+    mounted: function () {
+        if (this.$store.state.curations.items.length == 0) {
+            this.getAllCurationStatuses();
+            this.getAllRationales();
+        }
+        if (
+            !this.$store.state.features.transferEnabled
+            || !this.$store.state.features.sendToGciEnabled
+        ) {
+            this.getFeatures();
+        }
+        this.loading = false;
+    }
+}
+</script>
