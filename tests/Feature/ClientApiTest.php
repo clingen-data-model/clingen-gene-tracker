9c6d49ad2815da723b0560b85505681e65cf7121
setiadha <setiadha@unc.edu>
2025-06-23 00:54:09 -0400

    bulk look up

--- tests/Feature/ClientApiTest.php
+++ tests/Feature/ClientApiTest.php
@@ -89,4 +89,26 @@ class ClientApiTest extends TestCase
         $res->assertOk()
             ->assertJsonStructure(['success', 'data' => ['ontology', 'ontology_id', 'name']]);
     }
+
+    /** @test */
+    public function it_can_lookup_curations_by_gene_symbols_with_comma_and_newlines()
+    {
+        $textareaInput = "ARMC2, CFAP43, DNAH1, DNAH8,\nFANCM, CFTR, DNAH10";
+
+        $res = $this->postJsonToClientApi('client/v1/genes/curations', [
+            'gene_symbol' => $textareaInput
+        ]);
+        dd($res->json('data')[0]);
+        $res->assertOk()->assertJsonStructure([
+                'success',
+                'message',
+                'data' => [
+                    '*' => [
+                        'id',
+                        'gene',
+                        'available_phenotypes',
+                    ]
+                ]
+            ]);
+    }
 }
