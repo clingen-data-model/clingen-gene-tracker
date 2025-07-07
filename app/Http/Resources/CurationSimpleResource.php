ed903287c9422947965724fddddeb1831d74db5e
setiadha <setiadha@unc.edu>
2025-07-07 08:00:20 -0800

    updated curationsimplesearch return data plus incase an application has more than1200 genes, the default was set to 150

--- app/Http/Resources/CurationSimpleResource.php
+++ app/Http/Resources/CurationSimpleResource.php
@@ -17,9 +17,18 @@ class CurationSimpleResource extends JsonResource
         return [
             'id' => $this->id,
             'gene_symbol' => $this->gene_symbol,
+            'disease' => $this->disease ? $this->disease->name : null,
+            'mondo_id' => $this->disease ? $this->disease->mondo_id : null,
             'expert_panel' => $this->expertPanel ? (new ExpertPanelResource($this->expertPanel))->name : null,
+            'classification' => $this->classifications && $this->classifications->count()
+                                ? $this->classifications->pluck('name')->implode(', ')
+                                : '',
+            'curation_type' => $this->curationType ? $this->curationType->name : null,
             'current_status' => $this->currentStatus ? $this->currentStatus->name : null,
             'current_status_date' => Carbon::parse($this->currentStatusDate)->format('Y-m-d'),
+            'phenotype' => $this->phenotypes && $this->phenotypes->count()
+                            ? $this->phenotypes->map(fn($item) => "{$item->name} ({$item->mim_number})")->implode(', ')
+                            : '',
         ];
     }
 }
