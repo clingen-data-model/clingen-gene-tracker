a79e20a23f56e6c91a0eca4a6badfe4715813f43
Bradford Powell <bpow@drpowell.org>
2025-07-18 18:10:00 -0400

    be less verbose for missing resource exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,10 +4,19 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    public function register()
+    {
+        $this->renderable(function (NotFoundHttpException $e, $request) {
+            if ($request->is('api/*')) {
+                return response()->json(['error' => 'Resource not found'], 404);
+            }
+        });
+    }
     /**
      * A list of the exception types that are not reported.
      *
