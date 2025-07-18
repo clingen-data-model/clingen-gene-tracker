3217e8a79c8aae45414de00e95edc53d4b3f0e2c
Bradford Powell <bpow@drpowell.org>
2025-07-18 14:09:08 -0400

    revert changes to json exception rendering
    could be "fixup" rebase to commit "Update passport for client, token last for 3 mins, update table definition, and api routes"

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -49,12 +49,6 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        if ($request->expectsJson()) {
-            return response()->json([
-                'success' => false,
-                'message' => $exception->getMessage(),
-            ], method_exists($exception, 'getStatusCode') ? $exception->getStatusCode() : 500);
-        }
         return parent::render($request, $exception);
     }
 }
