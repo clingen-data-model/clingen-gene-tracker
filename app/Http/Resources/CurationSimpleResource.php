54518338c1fc65e6d7953306b009469398b8731e
setiadha-unc <setiadha@unc.edu>
2025-08-22 13:16:32 -0400

    enhance api for GPM

--- app/Http/Resources/CurationSimpleResource.php
+++ app/Http/Resources/CurationSimpleResource.php
@@ -14,22 +14,46 @@ class CurationSimpleResource extends JsonResource
      */
     public function toArray($request): array
     {
+        $disease    = $this->disease;
+        $moiRel     = $this->modeOfInheritance;
+        $classification_id = $classification = "";
+        if($this->currentClassification) {
+            $classification_id  = $this->currentClassification->id;
+            $classification     = $this->currentClassification->name;
+        }
+
+        $hash   = hash('sha256', $this->gene_symbol . $disease?->mondo_id . $this->moi?->abbreviation . $classification);
+
         return [
-            'id' => $this->id,
-            'gene_symbol' => $this->gene_symbol,
-            'disease' => $this->disease ? $this->disease->name : null,
-            'mondo_id' => $this->disease ? $this->disease->mondo_id : null,
-            'expert_panel' => $this->expertPanel ? (new ExpertPanelResource($this->expertPanel))->name : null,
-            'moi' => $this->moi ? $this->moi->abbreviation : null,
-            'classification' => $this->classifications && $this->classifications->count()
-                                ? $this->classifications->pluck('name')->implode(', ')
-                                : '',
-            'curation_type' => $this->curationType ? $this->curationType->name : null,
-            'current_status' => $this->currentStatus ? $this->currentStatus->name : null,
-            'current_status_date' => Carbon::parse($this->currentStatusDate)->format('Y-m-d'),
-            'phenotype' => $this->phenotypes && $this->phenotypes->count()
-                            ? $this->phenotypes->map(fn($item) => "{$item->name} ({$item->mim_number})")->implode(', ')
-                            : '',
+            'curation_id'           => $this->uuid,
+            'gene_symbol'           => $this->gene_symbol,
+
+            
+            "hgnc_id"               => $this->hgnc_id,
+            "hgnc_name"             => $this->hgnc_name,
+
+            'disease_name'          => $disease?->name,
+            'mondo_id'              => $disease?->mondo_id,
+
+            'expert_panel'          => $this->expertPanel?->name,
+
+            'moi'                   => $this->moi?->abbreviation,
+            'moi_name'              => $moiRel?->name,
+            'hp_id'                 => $moiRel?->hp_id,
+
+            'classification_id'     => $classification_id,
+            'classification'        => $classification,
+
+            'curation_status_id'    => $this->curation_status_id,
+            'curation_type'         => $this->curationType?->name,
+            'curation_status'       => $this->currentStatus?->name,
+
+            'date_approved'         => $this->currentStatusDate ?? null,
+
+            'phenotypes'            => $this->whenLoaded('phenotypes', fn () => $this->phenotypes->map(fn ($p)   => "{$p->name} ({$p->mim_number})")->implode(', '), ''),
+            'phenotypeIDs'          => $this->whenLoaded('phenotypes', fn () => $this->phenotypes->pluck('mim_number')->implode(', '), ''),
+
+            'checkKey'              => $hash
         ];
     }
 }
