6c9f8b7b960287325a4005d7b009b468b848defe
setiadha-unc <setiadha@unc.edu>
2026-02-16 14:35:09 -0500

    Update the logic and process for strean error. GT-72

--- app/DataExchange/Commands/CreateNotificationsForStreamErrors.php
+++ app/DataExchange/Commands/CreateNotificationsForStreamErrors.php
@@ -42,28 +42,81 @@ class CreateNotificationsForStreamErrors extends Command
      */
     public function handle()
     {
-        $allAffiliations = Affiliation::with(['expertPanel.coordinators'])->get();
         $admins = User::role('admin')->get();
 
-        $allCoordinators = $allAffiliations
-            ->pluck('expertPanel')
-            ->filter()
-            ->flatMap->coordinators
-            ->unique('id')
-            ->values();
-        
-        $groupedErrors = StreamError::unsent()
+        $errors = StreamError::unsent()
             ->with(['geneModel', 'diseaseModel', 'moiModel'])
+            ->get();
+
+        if ($errors->isEmpty()) { return; }
+
+        $groupedErrors = $errors->groupBy('affiliation_id');
+        $affiliationIds = $groupedErrors->keys()->filter()->values()->all();
+        $affiliationsByClingenId = Affiliation::with(['expertPanel.coordinators'])
+            ->whereIn('clingen_id', $affiliationIds)
             ->get()
-            ->groupBy('affiliation_id');
+            ->keyBy('clingen_id');
 
-        $recipients = $allCoordinators->isNotEmpty() ? $allCoordinators : $admins;
+        $parentLikeClingenIds = collect($affiliationIds)
+            ->filter(fn ($id) => str_starts_with((string) $id, '1'))
+            ->values();
 
-        $groupedErrors->each(function ($errors) use ($recipients) {
-            Notification::send($recipients, new StreamErrorNotification($errors));
-        });
-        
-        $groupedErrors->flatten()->each->markSent();
+        $parentLocalIds = $parentLikeClingenIds
+            ->map(fn ($clingenId) => $affiliationsByClingenId->get($clingenId))
+            ->filter()
+            ->pluck('id')
+            ->values()
+            ->all();
+
+        $childrenByParentId = collect();
+        if (!empty($parentLocalIds)) {
+            $childrenByParentId = Affiliation::with(['expertPanel.coordinators'])
+                ->whereIn('parent_id', $parentLocalIds)
+                ->get()
+                ->groupBy('parent_id');
+        }
+
+        $unmatched = collect();
+
+        foreach ($groupedErrors as $affiliationId => $groupErrors) {
+            $coordinators = collect();
+
+            $isParentLike = str_starts_with((string) $affiliationId, '1');
+
+            if ($isParentLike) {
+                $parentAff = $affiliationsByClingenId->get($affiliationId);
+
+                if ($parentAff) {
+                    $children = $childrenByParentId->get($parentAff->id, collect());
+
+                    $coordinators = $children
+                        ->flatMap(fn ($child) => optional($child->expertPanel)->coordinators ?? collect())
+                        ->unique('id')
+                        ->values();
+                }
+            } else {
+                $aff = $affiliationsByClingenId->get($affiliationId);
+
+                if ($aff && $aff->expertPanel) {
+                    $coordinators = $aff->expertPanel->coordinators ?? collect();
+                }
+            }
+
+            if ($coordinators->isNotEmpty()) {
+                Notification::send($coordinators, new StreamErrorNotification($groupErrors));
+            } else {
+                $unmatched = $unmatched->merge($groupErrors);
+            }
+        }
+
+        if ($unmatched->isNotEmpty()) {
+            Notification::send($admins, new StreamErrorNotification($unmatched));
+        }
+
+        $errors->each->markSent();
+
+        return 0;
     }
+
 
 }
