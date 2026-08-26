570b2f70460477513bb7bf584b4f3b707f5c4065
setiadha-unc <setiadha@unc.edu>
2026-08-26 15:35:39 -0400

    Fixed bugs on some components. L13+Vue3

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,16 +1,12 @@
 
 window._ = require('lodash');
-window.Popper = require('popper.js').default;
 
 /**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
+ * Load Bootstrap's JavaScript for non-Vue markup that uses Bootstrap's
+ * data attributes. Bootstrap 5 no longer requires jQuery.
  */
 
 try {
-    // window.$ = window.jQuery = require('jquery');
-
     require('bootstrap');
 } catch (e) {}
 
