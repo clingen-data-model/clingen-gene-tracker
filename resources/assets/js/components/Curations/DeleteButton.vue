d6e3e3f4821a226ff3cf44b2c960895513dd3b81
setiadha-unc <setiadha@unc.edu>
2026-08-27 22:55:29 -0400

    Migrate contained curation components to script setup. L13+Vue3

--- resources/assets/js/components/Curations/DeleteButton.vue
+++ resources/assets/js/components/Curations/DeleteButton.vue
@@ -11,62 +11,50 @@
     </button>
 </template>
 
-<script>
-    import { mapMutations, mapActions, mapGetters } from 'vuex'
+<script setup>
+import { computed } from 'vue'
+import { useRouter } from 'vue-router'
+import { useStore } from 'vuex'
 
-    export default {
-        props: {
-            curation: {
-                required: true,
-                type: Object
-            }
-        },
-        data() {
-            return {
-            }
-        },
-        computed: {
-            ...mapGetters({user: 'getUser'}),
-            title: function () {
-                let title = '';
-                if (this.curation && this.curation.gene_symbol) {
-                    title += this.curation.gene_symbol
-                    if (this.curation.mondo_id) {
-                        title += ' / ' + this.curation.mondo_id
-                    }
-                    if (this.curation.expert_panel) {
-                        title += ' for '+this.curation.expert_panel.name
-                    }
-                }
-                return title;
-            },
-        },
-        methods: {
-            ...mapActions('curations', {
-                destroyCuration: 'destroyItem'
-            }),
-           ...mapMutations('messages', [
-                'addInfo',
-                'addError'
-            ]),
-            deleteCuration() {
-                if (confirm("You're about to delete "+this.title+'. This can not be undone.  Are you sure you want to continue?')) {
-                    this.$router.push('/')
-                    const title = this.title
-                    this.destroyCuration(this.curation.id)
-                        .then(response => {
-                            this.addInfo(title + ' was successfully deleted.')
-                        })
-                        .catch((error) => {
-                            let msg = 'There was a problem deleting'+title;
-                            if (error.response.status == 403) {
-                                msg = 'You do not have permissions to delete curations.  Please contact an adminstrator to help you delete the curation.';
-                            }
-                            this.addError(msg)
-                        });
-                }
-            }
+const props = defineProps({
+    curation: {
+        required: true,
+        type: Object
+    }
+})
+
+const router = useRouter()
+const store = useStore()
+const user = computed(() => store.getters.getUser)
+const title = computed(() => {
+    let value = ''
+    if (props.curation && props.curation.gene_symbol) {
+        value += props.curation.gene_symbol
+        if (props.curation.mondo_id) {
+            value += ' / ' + props.curation.mondo_id
         }
-    
+        if (props.curation.expert_panel) {
+            value += ' for ' + props.curation.expert_panel.name
+        }
+    }
+    return value
+})
+
+function deleteCuration() {
+    if (confirm("You're about to delete " + title.value + '. This can not be undone.  Are you sure you want to continue?')) {
+        router.push('/')
+        const curationTitle = title.value
+        store.dispatch('curations/destroyItem', props.curation.id)
+            .then(() => {
+                store.commit('messages/addInfo', curationTitle + ' was successfully deleted.')
+            })
+            .catch((error) => {
+                let msg = 'There was a problem deleting' + curationTitle
+                if (error.response.status == 403) {
+                    msg = 'You do not have permissions to delete curations.  Please contact an adminstrator to help you delete the curation.'
+                }
+                store.commit('messages/addError', msg)
+            })
+    }
 }
 </script>
