d77bb9f354632114b46fba27ec7fd83139916689
setiadha-unc <setiadha@unc.edu>
2026-08-27 21:55:52 -0400

    Add curation form composable and migrate initial consumers. L13+Gt-73+Vue3

--- resources/assets/js/components/Curations/Forms/Documents.vue
+++ resources/assets/js/components/Curations/Forms/Documents.vue
@@ -3,27 +3,18 @@
         <documents-card :curation="curation"></documents-card>
     </div>
 </template>
-<script>
-import curationFormMixin from '../../../mixins/curation_form_mixin'
+<script setup>
+import { ref, watch } from 'vue'
+import useCurationForm from '../../../composables/useCurationForm'
 import DocumentsCard from '../Documents/DocumentsCard.vue'
 
-export default {
-    components: {
-        DocumentsCard,
-    },
-    mixins: [
-        curationFormMixin, // handles syncing of prop value to updatedCuration
-    ],
-    data() {
-        return {
-            page: 'documents',            
-            curation: {}
-        }
-    },
-    watch: {
-        updatedCuration() {
-            this.curation = (this.updatedCuration) ?  this.updatedCuration : {}
-        }
-    }
-}
+const props = defineProps(['modelValue', 'errors'])
+const emit = defineEmits(['update:modelValue'])
+const page = 'documents'
+const curation = ref({})
+const { updatedCuration } = useCurationForm(props, emit, page)
+
+watch(updatedCuration, (value) => {
+    curation.value = value || {}
+})
 </script>
