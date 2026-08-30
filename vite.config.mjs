0519886b6743d66cec8a54f2ec7efc8496d40bce
setiadha-unc <setiadha@unc.edu>
2026-08-30 10:25:01 -0400

    Clean up Vite and Sass build warnings. L13+Vue3

---
+++ vite.config.mjs
@@ -0,0 +1,47 @@
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/assets/sass/app.scss',
+                'resources/assets/js/app.js',
+            ],
+        }),
+        vue(),
+    ],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                quietDeps: true,
+            },
+        },
+    },
+    build: {
+        rolldownOptions: {
+            output: {
+                codeSplitting: {
+                    groups: [
+                        {
+                            name: 'ui-vendor',
+                            test: /node_modules[\\/](?:bootstrap(?:-vue-next)?|@floating-ui[\\/]|@vueuse[\\/]|reka-ui)[\\/]/,
+                            priority: 3,
+                        },
+                        {
+                            name: 'vue-vendor',
+                            test: /node_modules[\\/](?:@vue[\\/]|vue(?:-router|x)?[\\/])/,
+                            priority: 2,
+                        },
+                        {
+                            name: 'vendor',
+                            test: /node_modules[\\/]/,
+                            priority: 1,
+                        },
+                    ],
+                },
+            },
+        },
+    },
+})
